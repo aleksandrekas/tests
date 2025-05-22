4474c9f9d9c778a04f21f154b6bb86a9a3970e63
aleksandre <aleksandrekasrashvili@gmail.com>
2025-05-22 21:07:46 +0400

    just learned about hoisting

---
+++ hoisting.js
@@ -0,0 +1,33 @@
+
+
+testFn()
+secondFn()
+
+
+function testFn(){
+    console.log('testing function runss!!!!!')
+}
+
+function secondFn(){
+    console.log('second one runs as welll!!')
+}
+
+
+
+console.log(variable)
+
+var variable = 'Secretive Plotter' 
+
+
+
+
+
+// soo hoisting means when function still works even when it is called before it is defined js takes it and moves to top automatically,works only on normal fns
+// eventually  variables defined by var is hoisted as well but only definiton and not assigment soo you only get variable that is only defined 
+
+
+
+
+
+
+ 
